3b6bbecfda25417ecd816f8d7567a72bb3729f56
gauta <gautamgoklani135@gmail.com>
2022-09-18 18:02:06 +0530

    Team17

--- app/src/main/java/com/example/team17/MainActivity.java
+++ app/src/main/java/com/example/team17/MainActivity.java
@@ -1,14 +1,43 @@
 package com.example.team17;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
+    Button logOut_btn;
+    FirebaseAuth mAuth;
 
+    @SuppressLint("NonConstantResourceId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ActionBar aBar;
+        aBar = getSupportActionBar();
+        ColorDrawable cd = new ColorDrawable(Color.parseColor("#FFFFFF"));
+        Objects.requireNonNull(aBar).setBackgroundDrawable(cd);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        logOut_btn = findViewById(R.id.logOut_btn);
+
+        logOut_btn.setOnClickListener(view -> {
+            mAuth.signOut();
+            Toast.makeText(MainActivity.this, "Logout SuccessFUlly !", Toast.LENGTH_SHORT).show();
+            startActivity(new Intent(MainActivity.this, LoginActivity.class));
+            finish();
+        });
     }
 }
